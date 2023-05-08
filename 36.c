f08a017ce44773e31be28b8d86dfba3e43b6d349
Ric000 <kevinricardosanchezhernandez@gmail.com>
2023-05-08 17:51:11 -0600

    eje 36
    numero par o impar

---
+++ 36.c
@@ -0,0 +1,14 @@
+/*36 Kevin Ricardo Sanchez Hernadez 2BMPG*/
+	#include <stdio.h>
+	int main(int argc, char const *argv[/*27/04/23*/])
+	{
+		int n;
+		printf("ingresa un numero");scanf("%i",&n);
+		if (n%2==0){
+			printf("par %i",n);
+		}
+		else{
+			printf("impar");
+		}
+		return 0;
+	}

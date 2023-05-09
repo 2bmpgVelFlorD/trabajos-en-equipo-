1a125897662a62c0fe0611fc06c931343b10cd10
Ric000 <kevinricardosanchezhernandez@gmail.com>
2023-05-08 19:31:06 -0600

    Update 38.c

--- 38.c
+++ 38.c
@@ -5,8 +5,8 @@ int main(int argc, char const *argv[])
 	int num;
 	printf("Digite un numero del 1-4");scanf("%i",num);
 	switch (num){
-	case 1:
-		
+	case 1:printf("uno");break;
+	case 2:
 	}
 	return 0;
 }

afd7cd8a6a14144d365ed735798ce3d06352d5a5
Ric000 <kevinricardosanchezhernandez@gmail.com>
2023-05-08 18:22:10 -0600

    Update 37.c

--- 37.c
+++ 37.c
@@ -4,5 +4,10 @@ int main(int argc, char const *argv[])
 {
 	int numero;
 	printf("presiona el numero 1");scanf("%i",numero);
+	switch(numero){
+	case 1: 
+	case 2:printf("digito 1-2");break;
+	default:printf("error");
+	}
 	return 0;
 }

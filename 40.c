5b708b5b7dd618f0df7d7456a3ea04ff97d17cf6
Ric000 <kevinricardosanchezhernandez@gmail.com>
2023-05-09 18:17:11 -0600

    Update 40.c

--- 40.c
+++ 40.c
@@ -3,6 +3,13 @@
 #include <math.h>
 int main(int argc, char const *argv[])
 {
-	
+	int conn,num;
+	printf("ingresa un numero");scanf("i",&num);
+	conn=0;
+	do{
+		num=(num/10);
+		++conn;
+	} while (num!=0);
+	printf("%i",conn);
 	return 0;
 }

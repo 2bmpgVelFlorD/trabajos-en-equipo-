45c48fd1b747e540966d41fa7701bad7e44f6492
2bmpgVelFlorD <132585729+2bmpgVelFlorD@users.noreply.github.com>
2023-05-16 20:06:11 -0600

    Update ejer59.c

--- ejer59.c
+++ ejer59.c
@@ -1,5 +1,19 @@
 #include<stdio.h>
 intmain(){
+ float a;
+ float n;
+ float x;
+ printf("ingresa el numero del que desea saber sus valores\n"); 
+ scanf("%s", n);
+ a=1;
+ while(x!=n){
+  x=x+1;
+  a=x;
+  if (n%a==0){
+  }
+ 
+ } 
+ 
  
     
 

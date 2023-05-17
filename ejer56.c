a5a4eeb244fe6ac27ef2090d7f8c1e70aa138ef4
2bmpgVelFlorD <132585729+2bmpgVelFlorD@users.noreply.github.com>
2023-05-16 19:00:33 -0600

    Update ejer56.c

--- ejer56.c
+++ ejer56.c
@@ -1,3 +1,11 @@
 #include<stdio.h>
-int main(){
+intmain(){
     float acom;
+    float multiplicacion;
+    float x;
+    do{
+        acom  =  acom+1;
+        multiplicacion = 6*acom;
+        printf("%f/n", multiplicacion);
+        
+        

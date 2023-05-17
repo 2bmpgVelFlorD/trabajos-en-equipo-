94206f3328f3415f1eb44f422d56d8af4f2240b2
2bmpgVelFlorD <132585729+2bmpgVelFlorD@users.noreply.github.com>
2023-05-16 22:21:20 -0600

    Update ejer62.c

--- ejer62.c
+++ ejer62.c
@@ -1,5 +1,24 @@
 #include<stdio.h>
 #include<stdlib.h>
 intmain(){
+  int n;
+  int num;
+  int rpt;
+  float x;
+  num=(rand()%10);
+  printf("ingrese num de intentos\n");
+  printf("3\n");
+  printf("5\n");
+  printf("indefinido=(10)\n");
+  scanf("%i",&rpt);
+  switch(rpt){
+      case3:
+          while(x!=4 || n!=num){
+            printf("solo tienes 3 oportunidades\n")
+            
+          }
+      
+      
+  }
 
 }

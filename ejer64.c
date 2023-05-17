b8bda32a0d2478dd3e6a5faf1b0bfb5b235d80ff
2bmpgVelFlorD <132585729+2bmpgVelFlorD@users.noreply.github.com>
2023-05-16 23:53:54 -0600

    Update ejer64.c

--- ejer64.c
+++ ejer64.c
@@ -1,2 +1,17 @@
 #include<stdio.h>
-intmain();{
+intmain(){
+  float i;
+  float p;
+  float resultado;
+  float t;
+  printf("ingresa la cantidad de tu prestamo\n");
+  scanf("%f",&p);
+  printf("ingresa el tiempo\n");
+  scanf("%f",&t);
+  printf("INGRESA TASA DE INTERESES\n");
+  scanf("%f",&i);
+  resultado=(p*i*t)/100;
+  printf("el interes%fmeses es de%f\n", t, resultado);
+  return 0;
+}
+      

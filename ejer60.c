fd51a787f985a9ee955c36bbbeb91bc0cc51ff85
2bmpgVelFlorD <132585729+2bmpgVelFlorD@users.noreply.github.com>
2023-05-16 20:32:50 -0600

    Update ejer60.c

--- ejer60.c
+++ ejer60.c
@@ -1,4 +1,17 @@
 #include<stdio.h>
 intmain(){
+  float a;
+  float b;
+  a=0;
+  b=0;
+  while(a!=7){
+    while(){
+      printf("%.1f/%.1f\n",a,b);
+      b=b+1;
+    }
+    b=0;
+    a=a+1;
+  }
+  return 0;
 
 }

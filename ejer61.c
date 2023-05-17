46c422523f3ece1bfa5130f788fe9df0e703e477
2bmpgVelFlorD <132585729+2bmpgVelFlorD@users.noreply.github.com>
2023-05-16 21:16:32 -0600

    Update ejer61.c

--- ejer61.c
+++ ejer61.c
@@ -1,4 +1,16 @@
 #include<stdio.h>
 intmain(){
-  
+  float factorial;
+  float n;
+  float variable;
+  float x;
+  printf("ingresa el num, para pasar factorial\n");
+  scanf("%s",n);
+  variable=1;
+  while(){
+    x=x+1;
+    variable=variable*x;
+    factorial=variable*x;
+    printf("%f\n",x);;
+  }
 }

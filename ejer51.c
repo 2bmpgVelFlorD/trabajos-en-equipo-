a42da9a691a69a2fa18aaa9bbc120889c16d7c4f
2bmpgVelFlorD <132585729+2bmpgVelFlorD@users.noreply.github.com>
2023-05-17 23:34:18 -0600

    Update ejer51.c

--- ejer51.c
+++ ejer51.c
@@ -2,6 +2,31 @@
 #include<stdbool.h>
 #define MAX_STRLEN 256
 int main(){
+  int contador;
+  int respuesta;
+  char contraseña[MAX_STRLEN 256];
+  float saldo;
+  float deposito;
+  float retiro;
+  saldo=1000;
+  contador=1;
+    wile(contador<3 ; contador==3){
+    printf("escribe la contraseña\n");
+    scanf("%c",&contraseña);
+    if(contraseña=="fmmg"){
+      contador=4;
+      respuesta=0;
+      chucha=chucha<4;
+      chucho=chucho>4;
+      while(respuesta==chucha;respuesta==chucho){
+        printf();
+        
+      }   
+      
+    }
+      
+      
+     
 
 
 

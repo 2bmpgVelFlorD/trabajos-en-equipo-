46cf3b2509220dcad8b8aa3be6c47bb6bfeb5e22
2bmpgVelFlorD <132585729+2bmpgVelFlorD@users.noreply.github.com>
2023-05-17 23:07:38 -0600

    Update ejerburbuja.c

--- ejerburbuja.c
+++ ejerburbuja.c
@@ -5,4 +5,17 @@ main(){
   for (int i = 0 ; i < 10 ; i++){
     for (int j = 0 ; j < 10 ; j++){
       if(vector[j] > vector[j+1]){
+        aux = vector [j+1];
+        vector[j+1] = vector[j];
+        vector[j] = aux;
+      }
+    }
+  }
+  for(int i = 0; i < 10; i++){
+    printf("%d\t", vector[i]);
+  }
+  return 0;
+}
+
+  
         

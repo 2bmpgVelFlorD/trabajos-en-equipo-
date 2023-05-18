22470f75465039c8596afd9cd94102ef58fc5346
2bmpgVelFlorD <132585729+2bmpgVelFlorD@users.noreply.github.com>
2023-05-17 22:59:41 -0600

    Update ejerburbuja.c

--- ejerburbuja.c
+++ ejerburbuja.c
@@ -2,4 +2,7 @@
 main(){
   int vector[10]={10,4,67,24,6,25,7,100};
   int aux;
-  
+  for (int i = 0 ; i < 10 ; i++){
+    for (int j = 0 ; j < 10 ; j++){
+      if(vector[j] > vector[j+1]){
+        

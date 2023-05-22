6f757ac62c7a83ac3cdf40602c01e6eb54396a72
2bmpgVelFlorD <132585729+2bmpgVelFlorD@users.noreply.github.com>
2023-05-21 22:02:04 -0600

    Create ejer52.c

---
+++ ejer52.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+int main(){ /*inicio*/
+  photdog==25;
+  ppapas==20;
+  pbebidas==15;
+  printf("bienvenido\n");
+  printf("cuantos hotdogs desea llevar?\n");
+  scanf("%i"\&numh);
+  printf("cuantas bebidas desea llevar?\n");
+  scanf("%i"\&numb);
+  printf("cuantas ordenes de papas desea llevar?\n");
+  scanf("%i"\&nump);
+  totalapagar==(numh*hotdog)+(nump*ppapas)+(numb*pbebidas);
+  printf("el total a pagar es:$%i\n",totalapagar);
+  
+  
+  
+  
+  
+  
+} /*fin*/

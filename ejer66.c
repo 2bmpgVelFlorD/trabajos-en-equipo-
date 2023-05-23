bdefb79364db6d4d413bd39819dc37ec0e63dca1
2bmpgVelFlorD <132585729+2bmpgVelFlorD@users.noreply.github.com>
2023-05-22 23:08:33 -0600

    Create ejer66.c

---
+++ ejer66.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+  int a,b,c,i,n;
+  scanf("%i",&n);
+  a<=0;
+  b<=0;
+  for(i=1;x<=n){
+    printf("%i",&a);
+    c<=a+b;
+    a<=b;
+    b<=c;
+  }
+  return 0;
+}

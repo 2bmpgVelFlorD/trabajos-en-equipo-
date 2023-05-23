d663d6665a9a639d200085c1c61d92b5c16f2af0
Ric000 <kevinricardosanchezhernandez@gmail.com>
2023-05-22 19:55:15 -0600

    Create encuentra_1_num.c

---
+++ encuentra_1_num.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char const *argv[])
+{
+	int num; int res,valor;
+	do{
+		printf("introduce un numero entero\n");scanf("%i",&num);
+		if (num>100){
+			printf("el numero es mayor");
+			printf("repetir\n");scanf("%i",&valor);
+		} else if (num<100){
+			printf("el numero es menor\n");printf("repetir\n");scanf("%i",&valor);
+		}else{
+			printf("correcto\n");printf("repetir\n");scanf("%i",&valor);		
+		}
+	}
+while(valor !=0);printf("hasta pronto");
+	return 0;
+}

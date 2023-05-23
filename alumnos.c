27a3a74524f1895c89e3d23d1b356fea4c1a0805
Ric000 <131934028+Keviinric@users.noreply.github.com>
2023-05-23 10:28:27 -0600

    Rename Sin Nombre4.cpp to alumnos.c

--- alumnos.c
+++ alumnos.c
@@ -8,7 +8,7 @@ float calificaciones[5][5],promedio=0;
 printf("Programa que calcula el promedio de 5 calificaciones...\n");
 
 for(i=0;i<5;i++) {
-    printf("Dame la calificación %d:  ",i+1);
+    printf("Dame la calificaciÃ³n %d:  ",i+1);
     scanf("%f",&calificaciones[i]);
     promedio += calificaciones[i];swsss
     printf("\n");

8c871748a1e8eb92d73d15948ba4fa110765684b
Ric000 <kevinricardosanchezhernandez@gmail.com>
2023-05-23 15:51:22 -0600

    Update 43.c

--- 43.c
+++ 43.c
@@ -3,3 +3,5 @@ int main(int argc, char const *argv[/*29/04/23*/])
 { 
 	float sbruto,imss,vales,fnh,sneto;int num;
 	printf("ingresa el sueldo bruto");scanf("%f",&sbruto);
+	printf("que quieres hacer");scanf("%i",&num);
+	switch (num){

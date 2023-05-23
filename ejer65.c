9b761a428dfceeebd344711da5999f2fc1f6c596
2bmpgVelFlorD <132585729+2bmpgVelFlorD@users.noreply.github.com>
2023-05-22 22:58:38 -0600

    Create ejer65.c

---
+++ ejer65.c
@@ -0,0 +1,24 @@
+#include<sstdio.h>
+#define MAX_STRLEN 256
+int main(){ /*inicio*/
+  int ne,t,j;
+  char lineah,lineav[MAX_STRLEN];
+  printf("ingresa num de ne");
+  scanf("%i",&ne);
+  printf("*");
+  for(t=1;t<=10;t+=1){ /*para*/
+    printf("""\n");
+  
+  } /*finpara*/
+  for(j=1;j<=10;j+=1){ /*para*/
+    printf("""\n");
+  
+  } /*finpara*/
+  for(t=1;(ne+2)<=10;t+=1){ /*para*/
+    lineah=lineah+1;
+  
+  } /*finpara*/
+  printf("%i\n",lineah);
+  return 0;
+  
+} /*fin*/

00d9b0536409fef83b517bd6034d75930c48c9a0
Ric000 <kevinricardosanchezhernandez@gmail.com>
2023-05-23 16:05:13 -0600

    Update 44.c

--- 44.c
+++ 44.c
@@ -5,5 +5,8 @@ int main(int argc, char const *argv[])
 	char opcion;
 	do {
         printf("Ingrese una cantidad de dinero: ");
+        scanf("%f", &cantidad);
+
+        suma += cantidad;
 	return 0;
 }

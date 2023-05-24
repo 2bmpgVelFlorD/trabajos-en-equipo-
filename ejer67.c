a4cfa52217d3cdaaec0b6e011be52d28eef25eb7
2bmpgVelFlorD <132585729+2bmpgVelFlorD@users.noreply.github.com>
2023-05-23 18:53:06 -0600

    Create ejer67.c

---
+++ ejer67.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+  int a,b,c,n;
+  printf("ingrese un numero\n");
+  scanf("%i",&n);
+  a<=0;
+  b<=1;
+  for(i=1;i<=10){
+    printf("%i",a);
+    c<=a+b;
+    a<=b;
+    b<=c;
+  }
+return 0;
+    
+  
+}
+    
+   

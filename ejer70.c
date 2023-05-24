bbf8db12a8be87b7384ab27c0f8c4fec03385380
2bmpgVelFlorD <132585729+2bmpgVelFlorD@users.noreply.github.com>
2023-05-23 20:32:49 -0600

    Create ejer70.c

---
+++ ejer70.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+  int x;
+  float em;
+  int c[10];
+  for(x=0;x<=9;x+=1){
+    printf("ingresa la calificacion del alumno");
+    scanf("%i",&c(x));
+  }
+   for(x=0;x<=9;x+=1){
+     if(x=0){
+      em=c(x);
+     }else{
+       if(c(x)>em){
+        em=e(x);
+       }
+     }
+   } 
+  printf("la calificacion mas alta%i\n", em);
+  
+  
+return 0;
+}

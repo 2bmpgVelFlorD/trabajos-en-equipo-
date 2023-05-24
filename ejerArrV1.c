4725d0f7191d7c63db0909fa38789f558e55db6d
2bmpgVelFlorD <132585729+2bmpgVelFlorD@users.noreply.github.com>
2023-05-23 20:55:20 -0600

    Create ejerArrV1.c

---
+++ ejerArrV1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+  int t,i;
+  int num[3][4];
+  for(t=0;t<3;++t){
+    for(i=o;i<3;++i){
+      num[t][i]=(t*4)+i+1;
+    }
+  }
+  for(t=0;t<3;++t){
+    for(i=o;i<4;++i){
+      printf("%d",num[t][i]);
+      printf("\n");
+     
+    }
+  }
+return 0;
+}

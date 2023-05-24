7e6a175783daa2ac0e7b154c278c8b78306e9e9f
Ric000 <kevinricardosanchezhernandez@gmail.com>
2023-05-24 15:12:35 -0600

    Update 45.c

--- 45.c
+++ 45.c
@@ -4,3 +4,10 @@
 #define MAX_INTENTOS 3
 #define CONTRASENA_CORRECTA "1234"
 #define saldo 1000
+int main() {
+    char contrasena[20];
+    int intentos = 0,r,sacr,abonar,tca;
+    
+    while (intentos < MAX_INTENTOS) {
+        printf("Ingrese la contraseña: ");
+        scanf("%s", contrasena);

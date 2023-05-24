fa366e3fb17e9753382c984d69d45fda0595a59f
Ric000 <kevinricardosanchezhernandez@gmail.com>
2023-05-24 15:31:13 -0600

    Update 46.c

--- 46.c
+++ 46.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
+	float m;
+	int o;
 	do{
 		printf("ingrese su moneda");scanf("%i",&m);
+		printf("otra moneda");scanf("%i",&o);
+		s+=m
 	}
 	return 0;
 }

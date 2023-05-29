7b5978b2cb3b13e4c718aee6da418710cb0485db
Yayo34 <132587088+Yayo34@users.noreply.github.com>
2023-05-29 08:47:04 -0600

    Add files via upload

---
+++ 290523_5b.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int q;
+int resultado1;
+int resultado2;
+int main(){
+	int a;
+	a=124;q=1;
+	resultado1=a+q;
+	printf("r1:%d\n",resultado1);
+	{
+		int b;
+		int resultado3;
+		b=124;a=457;q=2;
+		resultado2=a+q+b;
+		printf("r1a:%d\n",resultado1);
+		printf("r1b:%d\n",resultado2);
+	}
+		
+		{
+		int c;
+		c=124;b=457;a=788;q=3;
+		resultado3=a+b+q+c;
+		printf("r3:%d\n",resultado3);
+		
+	}
+	
+	return 0;
+}

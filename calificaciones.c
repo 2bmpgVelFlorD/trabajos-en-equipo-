1562ec422931a0a57bbdd6fdbf6d85b4df0d675e
2bmpgVelFlorD <132585729+2bmpgVelFlorD@users.noreply.github.com>
2023-05-28 19:33:24 -0600

    Update calificaciones.c

--- calificaciones.c
+++ calificaciones.c
@@ -1,7 +1,24 @@
 #include<stdio.h>
-#define alumno 5
-#define calificaciones 5
-int main()
-{
+int sumarcalf(int calificaciones[]){
+  int suma=0
+    for(int i=0;i<5;i++){
+      suma+=calificaciones[i]{
+    }
+      return suma;
+    }
+  void mostrarresultado(int suma){
+    printf("la suma de las calificaciones es: %d\n", suma);
+  }
+}
+int main(){
+int calificaciones[5];
+  printf("ingrese las 5 calf de los alumnos\n");
+  for(int i=0;i<5;++i){
+    printf("alumno %d:", i+1);
+    scanf("%d", &calificaciones[i]);
+  }
+  int suma=sumarcalf(calificaciones);
+  mostrarresultado(suma);
+  return 0;
   
 }

cd28b6bef61f86189362264587b47b694f77d6b7
Yayo34 <132587088+Yayo34@users.noreply.github.com>
2023-05-29 08:02:42 -0600

    Add files via upload

---
+++ 29-05-23-3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int a,b,c;
+int main(){
+	int valor;
+	printf("tres valores");
+	scanf("%d %d %d\a", &a,&b,&c);
+	valor=a+b+c;
+	printf("Valor de a: %d\n",a);
+	printf("Valor de a: %d\n",b);
+	printf("Valor de a: %d\n",c);
+	printf("suma de las variables: %d", valor);
+	return 0;
+	}

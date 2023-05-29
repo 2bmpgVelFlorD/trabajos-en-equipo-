cc87156b62b112185e67ea60d23f38e5b636e63f
Ric000 <131934028+Keviinric@users.noreply.github.com>
2023-05-29 07:31:07 -0600

    Add files via upload

---
+++ minumero.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int minumero;
+int main (){
+	int j;
+	printf("numero favorito");
+	scanf("%d",&minumero);
+	int numerodealteraciones=10;
+	int i;
+	for(i=2;i<numerodealteraciones;i=i+2){
+		printf("interacion numero %d\n",i);
+	}
+	printf("cuanto vale aqui la variable i: %d\n",i);
+		printf("cuanto vale aqui la variable minumero:%d\n",minumero);
+			printf("cuanto vale aqui la variable de interaciones %d\n",numerodealteraciones);
+			return 0;
+}

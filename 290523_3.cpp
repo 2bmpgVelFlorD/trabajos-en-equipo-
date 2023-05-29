5a26d8ff0591bcf6e1ae35e7519ae0b1655890d2
Ric000 <131934028+Keviinric@users.noreply.github.com>
2023-05-29 08:00:57 -0600

    Add files via upload

---
+++ 290523_3.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int a,b,c;
+int main () {
+	int valor;
+	printf("tres valores");
+	scanf("%d %d %d\a ",&a,&b,&c);
+	valor=a+b+c;
+	printf("valor de a: %d\n",a);
+	printf("valor de b: %d\n",b);
+	printf("valor de c: %d\n",c);
+	printf("suma de las variables: %d",valor);
+	
+	return 0;
+}

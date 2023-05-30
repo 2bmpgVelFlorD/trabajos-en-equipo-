ac3e2395fb36e86c0ab32e1f77e090c06c105c51
Ric000 <kevinricardosanchezhernandez@gmail.com>
2023-05-30 16:13:25 -0600

    Update 48.c

--- 48.c
+++ 48.c
@@ -1,19 +1,21 @@
 #include<stdio.h>
 int main(int argc, char const *argv[])
 {
-	char s,e,e2,m,prom,h,prom2,p;
+	char s,h;
+	int e,e2,m,prom,prom2,p;
 	do{
 		printf("sexo y edad");scanf("%c %c",&s,&e);
-	if (s==m){
+	if (s=m){
 		++m;
 		++e;
 		prom==e/m;
-	}
-	if (s==h){
+	} else{
+	if (s=h){
 		++h;
 		++e2;
 		prom2=e2/h;
 	}
+}
 	++p;
 }while(p=15);
 	return 0;

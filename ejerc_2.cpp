d82f6997ab58f377a12e78de7be5379c6b6485da
Yayo34 <132587088+Yayo34@users.noreply.github.com>
2023-05-30 16:35:23 -0600

    Add files via upload

---
+++ ejerc_2.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+	int n1;
+	int n2;
+	int resta;
+	printf("ingrese un numero");
+	scanf("%d",&n1);
+	printf("ingrese otro numero");
+	scanf("%d",&n2);
+	resta= n1-n2;
+	printf("El resultado es: %d \n",resta);
+	return 0;
+}
+

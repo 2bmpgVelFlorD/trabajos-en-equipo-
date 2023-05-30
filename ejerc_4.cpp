51adbf49fe6ea7cb66cc368f7bc190a845d10af5
Yayo34 <132587088+Yayo34@users.noreply.github.com>
2023-05-30 16:38:17 -0600

    Add files via upload

---
+++ ejerc_4.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+	int n1;
+	int n2;
+	int multiplicacion;
+	printf("ingrese un numero");
+	scanf("%d",&n1);
+	printf("ingrese otro numero");
+	scanf("%d",&n2);
+	multiplicacion= n1*n2;
+	printf("El resultado es: %d \n",multiplicacion);
+	return 0;
+}
+

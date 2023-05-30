6559b77140708b456ba6571838b5d54f45fd08b3
Ric000 <kevinricardosanchezhernandez@gmail.com>
2023-05-30 17:57:03 -0600

    Update 48.c

--- 48.c
+++ 48.c
@@ -1,22 +1,30 @@
 #include<stdio.h>
 int main(int argc, char const *argv[])
 {
-	char s,h;
-	int e,e2,m,prom,prom2,p;
+	int e,e2,m,prom,prom2,p,sx,h;
 	while (p-16){
-		printf("sexo y edad");scanf("%c %c",&s,&e);
-	if (s=m){
+		printf("sexo y edad");scanf("%i",&sx);
+	if (sx=1){
 		++m;
 		++e;
 		prom==e/m;
 	} else{
-	if (s=h){
+	if (sx=2){
 		++h;
 		++e2;
 		prom2=e2/h;
 	}
 }
 	++p;
+}
+printf("el numero de mujeres es\n",m);
+printf("el numero hombres es\n",h );
+printf("la edad de las mujeres es \n",prom );
+printf("edad de los hombres es \n",prom2 );
+if(prom<prom2){
+printf("las mujeres son mayores\n");
+}else{
+	printf("los hombres tienen mayor edad");
 }
 	return 0;
 }

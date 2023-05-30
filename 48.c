511a04ff8cf766828da94f503b193bc91fd27c8a
Ric000 <kevinricardosanchezhernandez@gmail.com>
2023-05-30 15:59:30 -0600

    Create 48.c

---
+++ 48.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+	char s,e,e2,m,prom,h,prom2,p;
+	do{
+		printf("sexo y edad");scanf("%c %c",&s,&e);
+	if (s==m){
+		++m;
+		++e;
+		prom==e/m;
+	}
+	if (s==h){
+		++h;
+		++e2;
+		prom2=e2/h;
+	}
+	++p;
+}while(p=15);
+	return 0;
+}

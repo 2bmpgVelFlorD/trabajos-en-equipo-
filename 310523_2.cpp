ddb3ee58b56616419f3f5b600e0493bb0b12bb34
Ric000 <131934028+Keviinric@users.noreply.github.com>
2023-05-31 08:00:08 -0600

    Add files via upload

---
+++ 310523_2.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+	int a,b,c;
+	int codigo,fahrenheit,cord_x,cord_y;
+	codigo=3467;
+	fahrenheit=123.456;
+		cord_x=525;
+		cord_y=725;
+		a=b=c=45;
+		a=(b=(c=45));
+	printf("valor de codigo=%i\n",codigo);
+	printf("valor de fahrenheit=%i\n",fahrenheit);
+	printf("valor de cord_x=%i\n",cord_x);
+	printf("valor de cord_y=%i\n",cord_y);
+	printf("valor de a=%i\n",a);
+	printf("valor de b=%i\n ",b);
+	printf("valor de c=%i\n",c);
+	return 0;
+	
+}

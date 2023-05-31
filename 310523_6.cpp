74503b89e49673346d17e60f2bff82a923c9bccc
DiegoEduardo7 <132587413+DiegoEduardo7@users.noreply.github.com>
2023-05-31 08:51:13 -0600

    Add files via upload

---
+++ 310523_6.cpp
@@ -0,0 +1,18 @@
+/*310523
+Diego Eduardo Lopez Uribe
+titulo; */
+#include<stdio.h>
+int main(){
+	int edad,mayordeedad,juvenil;
+	scanf("%i",&edad);
+	if (edad>18){
+		printf("%i\n",mayordeedad);
+	}
+	if(edad>15||edad<=10){
+		printf("%i\n",juvenil);
+		
+	}
+	
+	
+	return 0;
+}

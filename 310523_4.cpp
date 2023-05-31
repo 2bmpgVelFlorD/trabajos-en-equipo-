88ee0537a96170a0e265abe46e5b749fee7c9da1
Ric000 <131934028+Keviinric@users.noreply.github.com>
2023-05-31 08:26:02 -0600

    Add files via upload

---
+++ 310523_4.cpp
@@ -0,0 +1,16 @@
+/*310523
+kevin ricardo
+titulo; */
+#include<stdio.h>
+int main(){
+	int m=99, n;
+	n= ++m;
+	
+	printf("m= %d, n=%d\n", m,n);
+	n= ++m;
+	printf("m= %d, n=%d\n", m,n);
+	printf("m= %%d\n",m++);
+	printf("m= %%d\n",++m);
+	
+	return 0;
+}

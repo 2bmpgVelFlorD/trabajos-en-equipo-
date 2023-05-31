b8b934f65573fa284a1ad06523cecca31a0d7fd4
Yayo34 <132587088+Yayo34@users.noreply.github.com>
2023-05-31 07:46:18 -0600

    Add files via upload

---
+++ 310523.cpp
@@ -0,0 +1,13 @@
+/*fecha 31/05/23
+yahir sanchez 
+titulo imprime el tamaño de valores de coma flotante y double */
+
+#include<stdio.h>
+int main()
+{
+	printf("elo tamaño de la varable de coma flotante es %d", sizeof (float));
+	printf("bytes\n");
+	printf("el tamaño de variable de doble presicion %d", sizeof(double));
+	printf("bytes\n");
+	return 0;
+}

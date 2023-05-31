165f0b6b8fa6b76af632c8f57382f459fcfcb1dd
Ric000 <kevinricardosanchezhernandez@gmail.com>
2023-05-30 18:13:19 -0600

    Update 51.c

--- 51.c
+++ 51.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 int main(int argc, char const *argv[])
-{
-	
+{	float p;
+	float p1;
+	float x;
+	do {
+		p1 = p1+1;
+		p = 4+p1;
+		printf("%.f\n",p);
+		x = x+1;
+	} while (x!=30);
+
 	return 0;
 }

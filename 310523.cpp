0a292dca67e0f242c6043bab53610de2e83cace6
Ric000 <131934028+Keviinric@users.noreply.github.com>
2023-05-31 07:43:17 -0600

    Add files via upload

---
+++ 310523.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	printf("elo tamaño de la varable de coma flotante es %d", sizeof (float));
+	printf("bytes\n");
+	printf("el tamaño de variable de doble presicion %d", sizeof(double));
+	printf("bytes\n");
+	return 0;
+}

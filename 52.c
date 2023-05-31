5b37d6d13aedbf063a3eec079b47e0a7e3541a99
Ric000 <kevinricardosanchezhernandez@gmail.com>
2023-05-30 18:36:03 -0600

    Update 52.c

--- 52.c
+++ 52.c
@@ -2,11 +2,11 @@
 int main(int argc, char const *argv[])
 {
 	int n,num1,smt;
-	printf("dime un numero");scanf("%I",n);
-	for (int x = 0; n < count; ++i)
+	printf("dime un numero");scanf("%i",n);
+	for (int x = 0; n<=x; ++x)
 	{
 	scanf("%i",&num1);
-	smt=smt*num1;
+	smt=smt+num1;
 	}
 	printf("%i",smt);
 	return 0;

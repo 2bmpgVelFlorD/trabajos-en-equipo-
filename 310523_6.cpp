9bd5921e53f728f16fdbe7f1e3dc1f0c1d42314a
Ric000 <131934028+Keviinric@users.noreply.github.com>
2023-05-31 08:50:00 -0600

    Add files via upload

---
+++ 310523_6.cpp
@@ -0,0 +1,18 @@
+/*310523
+kevin ricardo
+titulo; */
+#include<stdio.h>
+int main(){
+	int edad,mayordeedad,juvenil;
+	scanf("%i",&edad);
+	if (edad>18){
+		printf("%i\n",mayordeedad);
+	}
+	if(edad>15||edad<=10){
+		printf("%i\n",juvenil);
+		
+	}
+	
+	
+	return 0;
+}

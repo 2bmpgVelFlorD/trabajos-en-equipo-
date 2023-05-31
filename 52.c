ad6582053c9581017337472a0294720099ed15cf
Ric000 <kevinricardosanchezhernandez@gmail.com>
2023-05-30 18:36:37 -0600

    Update 52.c

--- 52.c
+++ 52.c
@@ -2,8 +2,8 @@
 int main(int argc, char const *argv[])
 {
 	int n,num1,smt;
-	printf("dime un numero");scanf("%i",n);
-	for (int x = 0; n<=x; ++x)
+	printf("dime un numero");scanf("%i",&n);
+	for (int x = 1; x<=n; ++x)
 	{
 	scanf("%i",&num1);
 	smt=smt+num1;

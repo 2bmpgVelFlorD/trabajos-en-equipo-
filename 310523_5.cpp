47239a3343eae0a6cc7471af2ebb6ae49f50d6fb
Ric000 <131934028+Keviinric@users.noreply.github.com>
2023-05-31 11:36:20 -0600

    Update 310523_5.cpp

--- 310523_5.cpp
+++ 310523_5.cpp
@@ -5,9 +5,9 @@ titulo; */
 
 int main()
 {
-	int n=5, t;
-	t=++n*--n;
-	printf("n= %d, t=%d\n", n, t);
-	printf("%d %d %d\n", ++n, ++n, n++);
+	int n=5, t;//la variable t=0
+	t=++n*--n;//la variable n tiene preincremento
+	printf("n= %d, t=%d\n", n, t);//la variable n su valor es igual a 5 y la variable t su valor es igual a 0
+	printf("%d %d %d\n", ++n, ++n, n++);//la variable n tiene dos preincrentos y un posincremento.
 	return 0;
 }

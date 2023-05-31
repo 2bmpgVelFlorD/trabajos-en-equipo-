893351e3303795ceeca2d9066f82989143dcc1d1
DiegoEduardo7 <132587413+DiegoEduardo7@users.noreply.github.com>
2023-05-31 08:35:04 -0600

    Add files via upload

---
+++ 310523_5.cpp
@@ -0,0 +1,13 @@
+/*310523
+kevin ricardo
+titulo; */
+#include<stdio.h>
+
+int main()
+{
+	int n=5, t;
+	t=++n*--n;
+	printf("n= %d, t=%d\n", n, t);
+	printf("%d %d %d\n", ++n, ++n, n++);
+	return 0;
+}

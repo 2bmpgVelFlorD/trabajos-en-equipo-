969795664d86e387beaef27a69ce7e957a520575
Ric000 <kevinricardosanchezhernandez@gmail.com>
2023-05-30 18:02:08 -0600

    Update 50.c

--- 50.c
+++ 50.c
@@ -3,6 +3,7 @@ int main(int argc, char const *argv[])
 {
 	float h,p,r,suma=0;
 	printf("cuantos hotdogs desea llevar");scanf("%f",&h);
-	printf("cuantas papas desea llevar");scanf
+	printf("cuantas papas desea llevar");scanf("%f",&p);
+	printf("cuantos refrescos desea llevar");
 	return 0;
 }

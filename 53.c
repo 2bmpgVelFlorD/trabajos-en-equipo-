104db4c71aefdce47b5880ca3f0e6625fab6f1c6
Ric000 <kevinricardosanchezhernandez@gmail.com>
2023-05-30 18:52:58 -0600

    Update 53.c

--- 53.c
+++ 53.c
@@ -8,5 +8,11 @@ int main(int argc, char const *argv[])
 	scanf("%f",&num2);
 	if (num2>0) {
 		do {
+			printf("%.f\n",num1);
+			x = x+1;
+		} while (x!=num2);
+		mul = num1*num2;
+		printf("%.f\n",mul);
+	}
 	return 0;
 }

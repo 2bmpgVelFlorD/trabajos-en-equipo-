449f4e19b69d47b9337f8a62b3524bc63d20618b
Yayo34 <132587088+Yayo34@users.noreply.github.com>
2023-05-31 08:29:19 -0600

    Add files via upload

---
+++ 310523_4.cpp
@@ -0,0 +1,16 @@
+/*310523
+yahir sanchez
+titulo; */
+#include<stdio.h>
+int main(){
+	int m=99, n;
+	n= ++m;
+	
+	printf("m= %d, n=%d\n", m,n);
+	n= ++m;
+	printf("m= %d, n=%d\n", m,n);
+	printf("m= %%d\n",m++);
+	printf("m= %%d\n",++m);
+	
+	return 0;
+}

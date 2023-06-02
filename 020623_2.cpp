64f769b4e4b94887f9d8ebe7dd54453c0bf91b66
Yayo34 <132587088+Yayo34@users.noreply.github.com>
2023-06-02 10:40:55 -0600

    Rename 020623_3if.cpp to 020623_2.cpp

--- 020623_2.cpp
+++ 020623_2.cpp
@@ -8,15 +8,15 @@ int main() {
     scanf("%d", &numero);
 
     if (numero > 0) {
-        printf("El número es mayor que cero.\n");
+        printf("El nÃºmero es mayor que cero.\n");
     }
 
     if (numero < 0) {
-        printf("El número es menor que cero.\n");
+        printf("El nÃºmero es menor que cero.\n");
     }
 
     if (numero == 0) {
-        printf("El número es igual a cero.\n");
+        printf("El nÃºmero es igual a cero.\n");
     }
 
     return 0;

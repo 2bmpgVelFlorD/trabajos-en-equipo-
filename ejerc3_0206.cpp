feb8537d866139fa6d7809eeb138f749b7e0e005
DiegoEduardo7 <132587413+DiegoEduardo7@users.noreply.github.com>
2023-06-02 10:59:10 -0600

    Create ejerc3_0206.cpp

---
+++ ejerc3_0206.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int numero1, numero2, mayor;
+
+    printf("Ingrese el primer número: ");
+    scanf("%d", &numero1);
+
+    printf("Ingrese el segundo número: ");
+    scanf("%d", &numero2);
+
+    if (numero1 > numero2) {
+        mayor = numero1;
+    } else {
+        mayor = numero2;
+    }
+
+    printf("El mayor número es: %d\n", mayor);
+
+    return 0;
+}

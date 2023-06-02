66a7cea4dd141bb8d0965ea6b39a3cb1139b4d75
DiegoEduardo7 <132587413+DiegoEduardo7@users.noreply.github.com>
2023-06-02 11:00:07 -0600

    Create ejerc4_0206.cpp

---
+++ ejerc4_0206.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int dividendo, divisor, cociente, residuo;
+
+    printf("Ingrese el dividendo: ");
+    scanf("%d", &dividendo);
+
+    printf("Ingrese el divisor: ");
+    scanf("%d", &divisor);
+
+    if (divisor != 0) {
+        cociente = dividendo / divisor;
+        residuo = dividendo % divisor;
+
+        puts("Resultado de la división:");
+        puts("Cociente: ");
+        printf("%d\n", cociente);
+        puts("Dividendo: ");
+        printf("%d\n", dividendo);
+        puts("Divisor: ");
+        printf("%d\n", divisor);
+        puts("Residuo: ");
+        printf("%d\n", residuo);
+    } else {
+        puts("Error: el divisor no puede ser cero.");
+    }
+
+    return 0;
+}

6cbcf7c4d5dc39863865c88cd867d4515423b7cf
Ric000 <131934028+Keviinric@users.noreply.github.com>
2023-06-02 09:33:51 -0600

    Add files via upload

---
+++ 020623_if.cpp
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main() {
+    float nota;
+    
+    
+    
+printf("Ingrese la nota (entre 1 y 10): ");
+    scanf("%f", &nota);
+    
+    if (nota > 0) {
+        printf("Aprobado\n");
+    }
+    
+    return 0;
+    }

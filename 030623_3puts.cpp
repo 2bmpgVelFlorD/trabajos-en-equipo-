b9332d01c0e9eeea24f3ab8df9341bfa5ef452ff
Ric000 <131934028+Keviinric@users.noreply.github.com>
2023-06-02 09:56:24 -0600

    Update and rename 030623_3.cpp to 030623_3puts.cpp

--- 030623_3puts.cpp
+++ 030623_3puts.cpp
@@ -4,19 +4,19 @@ int main() {
     int dividendo, divisor;
     int cociente, residuo;
     printf("Ingrese el dividendo: ");
-    scanf("%d", &dividendo);
+    scanf("%i", &dividendo);
     
     printf("Ingrese el divisor: ");
-    scanf("%d", &divisor);
+    scanf("%i, &divisor);
     
     if (divisor != 0) {
         cociente = dividendo / divisor;
         residuo = dividendo % divisor;
         
-        printf("Cociente: %d\n", cociente);
-        printf("Dividendo: %d\n", dividendo);
-        printf("Divisor: %d\n", divisor);
-        printf("Residuo: %d\n", residuo);
+        printf("Cociente: %in", cociente);
+        printf("Dividendo: %in", dividendo);
+        printf("Divisor: %in", divisor);
+        printf("Residuo: %in", residuo);
     }
     else {
         printf("Error: el divisor debe ser distinto de cero.\n");

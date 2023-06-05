a5f55e7575408a33564d861523cbbdfd2511f72e
DiegoEduardo7 <132587413+DiegoEduardo7@users.noreply.github.com>
2023-06-05 08:43:41 -0600

    Create 050623_2.cpp

---
+++ 050623_2.cpp
@@ -0,0 +1,23 @@
+int main(){
+	int opcion;
+	scanf("%i\n",opcion);
+	switch(opcion)
+	{
+	case 0:
+		printf("menor de tres....\n");
+		break;
+    case 1:
+    	printf("menor de tres....\n");
+    	break;
+    case 2:
+       printf("menor de tres....\n");
+    break;
+    case 3:
+    	printf("igual a 3\n");
+    	break;
+    default:
+    	printf("fuera de rango\n");
+	     
+		
+	}
+}

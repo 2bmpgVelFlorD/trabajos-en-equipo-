fa681a57f2f81a63501e8f057045bd19900b5c5f
Ric000 <131934028+Keviinric@users.noreply.github.com>
2023-06-05 10:47:01 -0600

    Create 050623_07.cpp

---
+++ 050623_07.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int num_blancos = 0, num_digitos = 0, num_distintos = 0;
+    char c;
+    
+    scanf("%c", &c);
+    
+    switch (c) {
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9':
+            num_digitos++;
+            printf("hola1 %i",num_digitos);
+        case ' ': case '\t': case '\n':
+            num_blancos++;
+            printf("hola2 %i",num_blancos);
+            break;
+        default:
+            num_distintos++;
+            printf("hola3 %i",num_distintos);
+            break;
+    }
+    
+    return 0;
+}

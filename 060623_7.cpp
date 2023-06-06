b436882e353ff020c7f293f2dc0b6f17a40985f3
Yayo34 <132587088+Yayo34@users.noreply.github.com>
2023-06-06 15:45:21 -0600

    Create 060623_7.cpp

---
+++ 060623_7.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()//con las llaves nos da el valor de 1 a 25 y sin ellas solo nos da el valor 1
+{
+	int contador=1;
+	while(contador<25)
+	    printf("%d\n",contador);
+	    contador++;
+	return 0;
+}

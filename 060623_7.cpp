a4ad4ccc66acb1f89edb6aa2cab398c475acee7f
DiegoEduardo7 <132587413+DiegoEduardo7@users.noreply.github.com>
2023-06-06 16:09:22 -0600

    Create 060623_7.cpp

---
+++ 060623_7.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{//sin llaves solo te da el valor 1, y con llaves te da del 1 al 25
+	int contador=1;
+	while (contador<25)
+	{
+		printf("%d\n",contador);
+		contador++;
+	}
+	return 0;
+}

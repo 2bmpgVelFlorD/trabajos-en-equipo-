86f594c92f7e699dd4905cbf753c97ac1ea3f25a
DiegoEduardo7 <132587413+DiegoEduardo7@users.noreply.github.com>
2023-06-06 10:10:16 -0600

    Create 060623_5.cpp

---
+++ 060623_5.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	/*entrada de datos numericos,centinela-1*/
+	int const centinela=-1;
+	int suma,cuenta,nota;
+	printf("Introduzca la primera nota");
+	scanf("%d",&nota);
+	while (nota!=centinela)
+	{
+		cuenta++;
+		suma+=nota;
+		printf("Introduzca la siguiente nota");
+		scanf("%d",&nota);
+	}//fin de while
+}

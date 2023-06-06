73b49c91e50862ef1ac10750fd6cc03344054224
Ric000 <131934028+Keviinric@users.noreply.github.com>
2023-06-06 09:59:48 -0600

    Add files via upload

---
+++ 060623_4.cpp
@@ -0,0 +1,11 @@
+/*una variante
+kevin ricardo*/
+#include<stdio.h>
+int main(){
+	int contador=1;
+	while (contador>100)//si la prueba logica es falsa no se ejecutan las instrucciones
+	{
+		printf("%d\n",contador);
+		contador++;
+	}
+}

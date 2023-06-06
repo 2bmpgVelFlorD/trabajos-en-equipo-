50161554703895391fc51a20fd68c6e85545ccc7
DiegoEduardo7 <132587413+DiegoEduardo7@users.noreply.github.com>
2023-06-06 10:01:02 -0600

    Create 060623_4.cpp

---
+++ 060623_4.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int contador=1;
+	while (contador>100)//si la prueba logica es falsa se ejecuta la accion.
+	{
+		printf("%d\n",contador);
+		contador++;
+	}
+	return 0;
+}

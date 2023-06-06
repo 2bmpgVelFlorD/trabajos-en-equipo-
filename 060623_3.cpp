45871ce85470d92b4ee78d702a59764a94760786
DiegoEduardo7 <132587413+DiegoEduardo7@users.noreply.github.com>
2023-06-06 09:52:12 -0600

    Create 060623_3.cpp

---
+++ 060623_3.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int contador=0;//inicializo la variable
+    while(contador<5)//prueba logica
+    {
+    contador++;//si verdadero realiza bloque de instrucciones
+	printf("%d\n",contador);	
+	}
+	printf("Terminado.contador:%d\n",contador);
+	return 0;
+}

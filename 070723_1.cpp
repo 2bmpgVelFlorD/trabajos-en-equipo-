4c8a10e7086e0b55d66b025390e59f5af67b45d5
Yayo34 <132587088+Yayo34@users.noreply.github.com>
2023-06-07 09:03:59 -0600

    Create 070723_1.cpp

---
+++ 070723_1.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+
+	int digito_leido=0;
+
+	char car;
+
+	while(!digito_leido){
+
+		printf("\nintroduzca un caracter\n");
+
+		scanf("%c",&car);
+
+		digito_leido=(('0'<=car))&&(('9'<=car));
+
+	}
+
+} 

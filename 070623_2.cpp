e4d13cd76e8592f4783f44f99a8edd66c583ea37
Yayo34 <132587088+Yayo34@users.noreply.github.com>
2023-06-07 09:04:50 -0600

    Add files via upload

---
+++ 070623_2.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main (){
+	float num=6,media,nu,n;
+	while(num>0)
+	{
+		printf("dame un numero");
+		scanf("%f",&n);
+		n+=
+		num--;
+	}	
+	media=n/6;
+	printf("%.2f",media);
+}

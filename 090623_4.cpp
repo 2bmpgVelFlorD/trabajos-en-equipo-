2ad3352326bb7b048ecbf720d9e4a5a08e99f4be
Yayo34 <132587088+Yayo34@users.noreply.github.com>
2023-06-09 10:41:00 -0600

    Add files via upload

---
+++ 090623_4.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	char opcion,s,S;
+	do{
+		puts("hola");
+		puts("desea otro tipo de saludo");
+		printf("a continuacion pulsa intro");
+		scanf("%s",&opcion);
+	}while("opcion sea igual a s",opcion==s,opcion==S);
+	puts("adios");
+	return 0;
+	
+}

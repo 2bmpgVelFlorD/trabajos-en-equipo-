ee29b6e7d8ecd297d96696490d11bab0027359b0
Yayo34 <132587088+Yayo34@users.noreply.github.com>
2023-06-09 10:30:34 -0600

    Add files via upload

---
+++ 090623_1.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main (){
+	
+	int fila_blancos,cuenta_as;
+	puts("");
+	for(;;){
+		printf("siempre asi terminams");
+	}
+	return 0;
+}
---
+++ 090623_2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	int n,suma=0;
+	for(n=1;n<=10;n++){
+		suma<=2*n;
+		printf("la suma de los 10 primeros numeros pares es: %d",suma);
+	}
+
+   return 0;
+}

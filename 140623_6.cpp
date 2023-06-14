0d55990d6123f129e30e263f3a2a25e8eebf0305
Yayo34 <132587088+Yayo34@users.noreply.github.com>
2023-06-14 12:19:04 -0600

    Create 140623_6.cpp

---
+++ 140623_6.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+	obtenercaracteres();
+	alfabetizar();
+	verletras();
+	return 0;
+}
+
+	int obtenercaracteres()
+{
+	return 0;
+}
+    int alfabetizar()
+{
+	return 0;
+	
+}
+    void verletras()
+{
+	return ;
+}   

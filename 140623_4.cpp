55b00e8adc85c20fcc7f2766f4b51b17442760b5
DiegoEduardo7 <132587413+DiegoEduardo7@users.noreply.github.com>
2023-06-14 08:36:31 -0600

    Create 140623_4.cpp

---
+++ 140623_4.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+   int n = 50, i, suma = 0;
+   for (i = 1; i <= n; i++) {
+   	printf("%i\n",i);
+      suma += i;
+   }
+   printf("La suma de los primeros %d enteros es: %d\n", n, suma);
+   return 0;
+}

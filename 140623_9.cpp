0b5e18fd3c000225d06940fff88561e585d8c424
Ric000 <131934028+Keviinric@users.noreply.github.com>
2023-06-14 12:42:37 -0600

    Create 140623_9.cpp

---
+++ 140623_9.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int max(int x, int y){
+
+	if(x<y)	return y;
+
+	else
+
+	return x;
+
+}
+
+int main(){
+
+	int m,n;
+
+	do{
+
+		scanf("%d %d",&m, &n);
+
+		printf("maximo de %d y %d es %d\n",m,n,max (m,n));
+
+	}while(m!=0);
+
+	return 0;
+
+}

632fdcad0f25e439bd5fe649386e48da69c1e4d2
Ric000 <131934028+Keviinric@users.noreply.github.com>
2023-06-14 08:00:44 -0600

    Add files via upload

---
+++ 140623_5.cpp
@@ -0,0 +1,4 @@
+dterminar si un numero dado leido del teclado es primo o no repitiendo la operacion un numero de veces determinado 
+por otro numero asignado por teclado
+2 controlado un numero de veces controlado por centinela y controlar n de repeticiones por bandera
+.- escribir un programa que calcule loa suma de los primeros 50 n enteros

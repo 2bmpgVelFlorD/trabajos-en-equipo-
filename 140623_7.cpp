58b7f4ad4619039e23e82005e87471feeff8e83e
Yayo34 <132587088+Yayo34@users.noreply.github.com>
2023-06-14 12:20:00 -0600

    Create 140623_7.cpp

---
+++ 140623_7.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void func1(void){
+	puts("segunda funcion");
+	return ;
+}
+int func2(){
+	puts("tercera funcion");
+	return 0;
+}
+
+int main(){
+	puts("Primera funcion main");
+func1();
+func2();
+puts("Ultima instruccion en main");
+return 0;
+}

5d91f6c56a4343f9d58af35a85c90ad9b7e176de
DiegoEduardo7 <132587413+DiegoEduardo7@users.noreply.github.com>
2023-06-14 12:19:49 -0600

    Create 140623_6.cpp

---
+++ 140623_6.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main ()
+int obtenercaracteres()
+{
+	obtenercaracteres();
+	alfabetizar();
+	verletras;
+	return 0;
+}
+{
+	return 0;
+}
+	int alfabetizar()
+{
+	return 0;
+}
+void verletras()
+{
+	return;
+}

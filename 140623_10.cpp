b70719793458c9423ecb6ed1d1c93ab1b25194d9
Yayo34 <132587088+Yayo34@users.noreply.github.com>
2023-06-14 12:41:15 -0600

    Create 140623_10.cpp

---
+++ 140623_10.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int num_elementos=3;
+	int indice;
+	float total=0.0;
+	
+	printf("\n \t introduce %d numeros reales \n",num_elementos);
+	for (indice=0;indice<num_elementos;indice++)
+	{
+		float x;
+		scanf("%f",&x);
+		total+=x;
+	}
+	printf("%f",total);
+	return 0;
+}

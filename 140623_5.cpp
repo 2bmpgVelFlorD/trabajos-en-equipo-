00a1b69d48538abcfcc3a9209f3655af1f158b65
Yayo34 <132587088+Yayo34@users.noreply.github.com>
2023-06-14 08:44:14 -0600

    Update 140623_5.cpp

--- 140623_5.cpp
+++ 140623_5.cpp
@@ -1,4 +1,15 @@
-dterminar si un numero dado leido del teclado es primo o no repitiendo la operacion un numero de veces determinado 
-por otro numero asignado por teclado
-2 controlado un numero de veces controlado por centinela y controlar n de repeticiones por bandera
-.- escribir un programa que calcule loa suma de los primeros 50 n enteros
+#include <stdio.h>
+int main()
+{
+    int numero=0;
+	int c,x; 
+    do
+    {
+        printf("\nNumero ");
+        scanf(" %d",&numero);
+    x+=numero;
+    } while(numero!=-1);
+    printf("\nNumero %d",x);
+    
+    return 0;
+}
